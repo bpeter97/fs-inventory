e0b9e591422e76d513d1ce05f1afe9fe93e5c462
Brian Peter <brian.lee.peter@gmail.com>
2021-07-07 22:44:15 -0700

    Create Inspection Model

---
+++ src/server/models/Inspection.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Define the Inspection model.
+const InspectionSchema = new Schema({
+    sections: [],
+    inspectior: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: "User"
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    num_of_stories: {
+        type: Number,
+        required: true
+    },
+    year_built: {
+        type: Number,
+        required: false
+    },
+    people_at_inspection: {
+        type: String,
+        required: true
+    },
+    date_of_inspection: {
+        type: Date,
+    },
+});
+
+// Export the Inspection model.
+module.exports = Inspection = mongoose.model("inspection",InspectionSchema);
