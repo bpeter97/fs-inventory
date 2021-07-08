76905a43228394a946f278e4e9476f3e3b183e63
Brian Peter <brian.lee.peter@gmail.com>
2021-07-07 22:39:42 -0700

    Create Job model

---
+++ src/server/models/Job.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Define the Job model.
+const JobSchema = new Schema({
+    inspections: [],
+    client: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: "Client"
+    },
+    adress: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    state: {
+        type: String,
+        required: true
+    },
+    zipcode: {
+        type: String,
+        required: true
+    },
+    date_created: {
+        type: Date,
+    },
+});
+
+// Export the Job model.
+module.exports = Job = mongoose.model("job",JobSchema);
