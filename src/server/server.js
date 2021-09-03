627aedd8dfefe46b93324f2efc5d7a5c67834c19
Brian Peter <brian.lee.peter@gmail.com>
2021-09-03 09:06:26 -0700

    Correct server.js

--- src/server/server.js
+++ src/server/server.js
@@ -41,9 +41,6 @@ if (process.env.NODE_ENV === "production") {
 const app = express();
 
 app.use(express.static(path.join(__dirname, "./../client/build")));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './../client/build'))
-})
 
 // Middleware for body-parser
 app.use(bodyParser.urlencoded({ extended: false }));
