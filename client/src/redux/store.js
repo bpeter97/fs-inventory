a01860f3b204dc97970e441450ca412c10a842ff
Brian Peter <brian.lee.peter@gmail.com>
2021-09-20 08:16:40 -0700

    comment out devtools

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,8 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import { createBrowserHistory } from "history";
-import thunk from "redux-thunk";
-import multi from "redux-multi";
-import rootReducer from "./reducers";
+import { createStore, applyMiddleware, compose } from 'redux';
+import { createBrowserHistory } from 'history';
+import thunk from 'redux-thunk';
+import multi from 'redux-multi';
+import rootReducer from './reducers';
 
 const initialState = {};
 
@@ -11,12 +11,12 @@ export const history = createBrowserHistory();
 const middleware = [thunk, multi];
 
 const store = createStore(
-  rootReducer(history),
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+	rootReducer(history),
+	initialState,
+	compose(
+		applyMiddleware(...middleware),
+		// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	),
 );
 
 export default store;
